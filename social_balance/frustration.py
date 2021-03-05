89d04862c4c1c2262d6e36df5da88aadd6692c5d
morpheusthewhite <zffromGerace@hotmail.it>
2021-03-05 19:26:46 +0100

    feat(frustration): add branching priority optimization

--- social_balance/frustration.py
+++ social_balance/frustration.py
@@ -2,16 +2,24 @@ import gurobipy as gp
 import numpy as np
 
 
-def and_model(n_vertexes: int, edges: np.array) -> int:
+def and_model(
+    n_vertices: int,
+    edges: np.array,
+    optimize: bool = True,
+    degrees: np.array = None,
+) -> int:
     """calculate frustration using XOR formulation (no optimization)
 
     Args:
-        n_vertexes (int): number of vertexes in the graph. Vertices index in
-        the edges need to be in [0, n_vertexes-1]
+        n_vertices (int): number of vertices in the graph. Vertices index in
+        the edges need to be in [0, n_vertices-1]
         edges (np.array): a (n_edges, 3) numpy array, each row representing an
         edge:
             - the first and second elements are the vertices index
             - the third element is the edge sign (+1 or -1)
+        optimize: if true use optimizations
+        degrees: the degrees of the nodes. if optimization is true and they are
+        not provided they will be computed
 
     Returns:
         int: the number of frustrated edges
@@ -19,13 +27,15 @@ def and_model(n_vertexes: int, edges: np.array) -> int:
     model = gp.Model("and_unoptimized")
     vertices_variables = [
         model.addVar(vtype=gp.GRB.BINARY, name=f"x_{i}")
-        for i in range(n_vertexes)
+        for i in range(n_vertices)
     ]
     model.update()
 
-    # not really needed
-    #  edges_variables = np.ndarray((n_vertexes, n_vertexes), dtype=object)
     objective = 0
+    # if optimization is requested and degrees are not provided they are
+    # computed in the cycle
+    if degrees is None and optimize:
+        degrees_ = np.zeros((n_vertices), dtype=np.int32)
 
     for edge in edges:
         vertex1 = edge[0]
@@ -33,6 +43,11 @@ def and_model(n_vertexes: int, edges: np.array) -> int:
 
         vertex2 = edge[1]
         x_j = vertices_variables[vertex2]
+
+        # if not provided compute the degrees
+        if degrees is None and optimize:
+            degrees_[vertex1] += 1
+            degrees_[vertex2] += 1
 
         sign = edge[2]
 
@@ -50,22 +65,37 @@ def and_model(n_vertexes: int, edges: np.array) -> int:
         f_ij = (1 - sign) / 2 + sign * (x_i + x_j - 2 * x_ij)
         objective += f_ij
 
+    if degrees is None:
+        degrees = degrees_
+
+    for i, degree in enumerate(degrees):
+        x_i = vertices_variables[i]
+        x_i.branchPriority = int(degree)
+
     model.setObjective(objective, gp.GRB.MINIMIZE)
     model.optimize()
 
     return model.objVal
 
 
-def xor_model(n_vertexes: int, edges: np.array) -> int:
+def xor_model(
+    n_vertices: int,
+    edges: np.array,
+    optimize: bool = True,
+    degrees: np.array = None,
+) -> int:
     """calculate frustration using AND formulation (no optimization)
 
     Args:
-        n_vertexes (int): number of vertexes in the graph. Vertices index in
-        the edges need to be in [0, n_vertexes-1]
+        n_vertices (int): number of vertices in the graph. Vertices index in
+        the edges need to be in [0, n_vertices-1]
         edges (np.array): a (n_edges, 3) numpy array, each row representing an
         edge:
             - the first and second elements are the vertices index
             - the third element is the edge sign (+1 or -1)
+        optimize: if true use optimizations
+        degrees: the degrees of the nodes. if optimization is true and they are
+        not provided they will be computed
 
     Returns:
         int: the number of frustrated edges
@@ -73,13 +103,15 @@ def xor_model(n_vertexes: int, edges: np.array) -> int:
     model = gp.Model("and_unoptimized")
     vertices_variables = [
         model.addVar(vtype=gp.GRB.BINARY, name=f"x_{i}")
-        for i in range(n_vertexes)
+        for i in range(n_vertices)
     ]
     model.update()
 
-    # not really needed
-    #  edges_variables = np.ndarray((n_vertexes, n_vertexes), dtype=object)
     objective = 0
+    # if optimization is requested and degrees are not provided they are
+    # computed in the cycle
+    if degrees is None and optimize:
+        degrees_ = np.zeros((n_vertices), dtype=np.int32)
 
     for edge in edges:
         vertex1 = edge[0]
@@ -87,6 +119,11 @@ def xor_model(n_vertexes: int, edges: np.array) -> int:
 
         vertex2 = edge[1]
         x_j = vertices_variables[vertex2]
+
+        # if not provided compute the degrees
+        if degrees is None and optimize:
+            degrees_[vertex1] += 1
+            degrees_[vertex2] += 1
 
         sign = edge[2]
 
@@ -104,22 +141,36 @@ def xor_model(n_vertexes: int, edges: np.array) -> int:
 
         objective += f_ij
 
+    if degrees is None:
+        degrees = degrees_
+
+    for i, degree in enumerate(degrees):
+        x_i = vertices_variables[i]
+        x_i.branchPriority = int(degree)
+
     model.setObjective(objective, gp.GRB.MINIMIZE)
     model.optimize()
 
     return model.objVal
 
 
-def abs_model(n_vertexes: int, edges: np.array) -> int:
+def abs_model(
+    n_vertices: int,
+    edges: np.array,
+    optimize: bool = True,
+    degrees: np.array = None,
+) -> int:
     """calculate frustration using ABS formulation (no optimization)
 
     Args:
-        n_vertexes (int): number of vertexes in the graph. Vertices index in
-        the edges need to be in [0, n_vertexes-1]
+        n_vertices (int): number of vertices in the graph. Vertices index in
+        the edges need to be in [0, n_vertices-1]
         edges (np.array): a (n_edges, 3) numpy array, each row representing an
         edge:
             - the first and second elements are the vertices index
             - the third element is the edge sign (+1 or -1)
+        optimize: if true use optimizations
+        degrees: the degrees of the nodes. if optimization is true and they are
 
     Returns:
         int: the number of frustrated edges
@@ -127,13 +178,15 @@ def abs_model(n_vertexes: int, edges: np.array) -> int:
     model = gp.Model("and_unoptimized")
     vertices_variables = [
         model.addVar(vtype=gp.GRB.BINARY, name=f"x_{i}")
-        for i in range(n_vertexes)
+        for i in range(n_vertices)
     ]
     model.update()
 
-    # not really needed
-    #  edges_variables = np.ndarray((n_vertexes, n_vertexes), dtype=object)
     objective = 0
+    # if optimization is requested and degrees are not provided they are
+    # computed in the cycle
+    if degrees is None and optimize:
+        degrees_ = np.zeros((n_vertices), dtype=np.int32)
 
     for edge in edges:
         vertex1 = edge[0]
@@ -141,6 +194,11 @@ def abs_model(n_vertexes: int, edges: np.array) -> int:
 
         vertex2 = edge[1]
         x_j = vertices_variables[vertex2]
+
+        # if not provided compute the degrees
+        if degrees is None and optimize:
+            degrees_[vertex1] += 1
+            degrees_[vertex2] += 1
 
         sign = edge[2]
 
@@ -156,6 +214,13 @@ def abs_model(n_vertexes: int, edges: np.array) -> int:
 
         f_ij = e_ij + h_ij
         objective += f_ij
+
+    if degrees is None:
+        degrees = degrees_
+
+    for i, degree in enumerate(degrees):
+        x_i = vertices_variables[i]
+        x_i.branchPriority = int(degree)
 
     model.setObjective(objective, gp.GRB.MINIMIZE)
     model.optimize()
