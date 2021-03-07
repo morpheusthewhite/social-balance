a6572d030abf24e440788f491b33add58be8916e
morpheusthewhite <zffromGerace@hotmail.it>
2021-03-07 11:27:36 +0100

    refactor(main): replace np.array with python lists

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import numpy as np
 from social_balance import frustration_model
 
 
 def main():
     model = "xor"
 
-    edges1 = np.array([[0, 1, 1], [2, 1, 1], [2, 0, 1]])
+    edges1 = [[0, 1, 1], [2, 1, 1], [2, 0, 1]]
     n_frustrated1 = frustration_model(3, edges1, False, model=model)
 
     print("=" * 20)
@@ -14,7 +13,7 @@ def main():
     print("=" * 20)
     print("-" * 10)
 
-    edges2 = np.array([[0, 1, 1], [2, 1, 1], [2, 0, -1]])
+    edges2 = [[0, 1, 1], [2, 1, 1], [2, 0, -1]]
     n_frustrated2 = frustration_model(3, edges2, False, model=model)
 
     print("=" * 20)
